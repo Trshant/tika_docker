3fa2b4f36793e73952906b47e8827fef428ee948
Trshant Bhat <trshant@gmail.com>
2025-05-14 23:17:41 +0200

    create the versioning in Alembic

--- data_schema/document.py
+++ data_schema/document.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, LargeBinary, JSON
+from sqlalchemy import Column, Integer, String, LargeBinary, JSON, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import BINARY
 
@@ -10,6 +10,6 @@ class Document(Base):
 
     id = Column(BINARY(16), primary_key=True)
     mdhash = Column(String(64), unique=True, nullable=False)
-    metadata = Column(JSON)
-    filepath = Column(String(255), nullable=False, index=True)
-    text_content = Column(String)  # Or TEXT, depending on your needs
+    meta_data = Column(JSON)
+    file_path = Column(String(255), nullable=False, index=True)
+    text_content = Column(Text)  # Or TEXT, depending on your needs
